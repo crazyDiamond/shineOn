f631239f8f6a724156762125257ebe9526683485
crazyDiamond <jeeva.ganeshan@gmail.com>
2010-05-27 22:43:58 -0400

    introduced pledge controller

---
+++ spec/controllers/pledge_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe PledgeController do
+
+  #Delete this example and add some real ones
+  it "should use PledgeController" do
+    controller.should be_an_instance_of(PledgeController)
+  end
+
+end
